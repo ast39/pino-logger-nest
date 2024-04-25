42cbc2688059b7cd62752ee9b9176b51e84f3f1b
timur <kurbanov.t.94@yandex.ru>
2024-04-25 14:28:14 +0500

    add test

--- src/common/logger/my-logger.module.ts
+++ src/common/logger/my-logger.module.ts
@@ -1,8 +1,37 @@
 import { Module } from '@nestjs/common';
 import { MyLoggerService } from './my-logger.service';
+import {LoggerModule} from "nestjs-pino";
+import pino from 'pino';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        serializers: {
+          req: (req) => ({
+            body: req['raw']['body'],
+            url: req['url'],
+            headers: {
+              host: req['raw']['headers']['host']
+            },
+          }),
+          res: (res) => ({
+            statusCode: res['statusCode'],
+          }),
+          err: (err) => ({
+            err
+          })
+        },
+        stream: pino.destination({
+          dest: './src/common/logger/logs/test.log', // omit for stdout
+          minLength: 4, // Buffer before iwriting
+          sync: false,
+          mkdir: true,
+          append: true
+        }),
+      },
+    }),
+  ],
   controllers: [],
   providers: [MyLoggerService],
   exports: [MyLoggerService],
